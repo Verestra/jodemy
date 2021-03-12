5782b7ec2c80f802fb77b90acab06af21245f756
DarkEmBasSy666 <67989743+jganz6@users.noreply.github.com>
2021-03-12 19:19:27 +0700

    fix dashboard and index.js

--- script/index.js
+++ script/index.js
@@ -7,15 +7,13 @@ function openNav() {
     if (status === true) {
         status = false;
         navSide.style.left = "-20px";
-        btnNav.style.marginLeft = "230px";
         navSide.style.display = "flex";
-        navSide.style.zIndex = "2";
-        navSide.style.top = "-20px";
+        navSide.style.zIndex = "4";
+        navSide.style.bottom = "-20px";
     } else {
         status = true;
         navSide.style.left = null;
-        btnNav.style.marginLeft = null;
-        navSide.style.top = null;
+        navSide.style.bottom = null;
         navSide.style.display = null;
         navSide.style.zIndex = null;
     }
@@ -27,18 +25,16 @@ function openChat() {
     if (statusChat === true) {
         statusChat = false;
         chatSide.style.right = "-20px";
-        btnChat.style.left = "0";
         chatSide.style.display = "flex";
-        chatSide.style.zIndex = "3";
-        chatSide.style.top = "-20px";
+        chatSide.style.zIndex = "5";
+        chatSide.style.bottom = "-20px";
         chatSide.style.width = "calc(100vw - 60px)";
     } else {
         statusChat = true;
         chatSide.style.right = null;
-        btnChat.style.left = null;
         chatSide.style.display = null;
         chatSide.style.zIndex = null;
-        chatSide.style.top = null;
+        chatSide.style.bottom = null;
         chatSide.style.width = null;
     }
 }
