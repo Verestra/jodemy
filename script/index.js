f8a8a879547110a768e909996e0e51ac54f27eae
DarkEmBasSy666 <67989743+jganz6@users.noreply.github.com>
2021-03-12 12:47:17 +0700

    add function for button nav and add javascript file

---
+++ script/index.js
@@ -0,0 +1,15 @@
+let status = true;
+
+function openNav() {
+    btnNav = document.getElementById("btn-nav");
+    navSide = document.getElementById("nav-side");
+    if (status === true) {
+        status = false;
+        navSide.style.left = "20px";
+        btnNav.style.marginLeft = "250px";
+    } else {
+        status = true;
+        navSide.style.left = "-250px";
+        btnNav.style.marginLeft = "0px";
+    }
+}
