3a809d2506188d5d58bd6457bdf7db06853e1ecc
DarkEmBasSy666 <67989743+jganz6@users.noreply.github.com>
2021-03-12 19:21:52 +0700

    index.js update

--- script/index.js
+++ script/index.js
@@ -7,13 +7,15 @@ function openNav() {
     if (status === true) {
         status = false;
         navSide.style.left = "-20px";
+        btnNav.style.marginLeft = "230px";
         navSide.style.display = "flex";
         navSide.style.zIndex = "4";
-        navSide.style.bottom = "-20px";
+        navSide.style.top = "-20px";
     } else {
         status = true;
         navSide.style.left = null;
-        navSide.style.bottom = null;
+        btnNav.style.marginLeft = null;
+        navSide.style.top = null;
         navSide.style.display = null;
         navSide.style.zIndex = null;
     }
@@ -25,16 +27,18 @@ function openChat() {
     if (statusChat === true) {
         statusChat = false;
         chatSide.style.right = "-20px";
+        btnChat.style.left = "0";
         chatSide.style.display = "flex";
         chatSide.style.zIndex = "5";
-        chatSide.style.bottom = "-20px";
+        chatSide.style.top = "-20px";
         chatSide.style.width = "calc(100vw - 60px)";
     } else {
         statusChat = true;
         chatSide.style.right = null;
+        btnChat.style.left = null;
         chatSide.style.display = null;
         chatSide.style.zIndex = null;
-        chatSide.style.bottom = null;
+        chatSide.style.top = null;
         chatSide.style.width = null;
     }
 }
