03eed274c9735ad7fe62a55fb6e0b192af1e57b4
DarkEmBasSy666 <67989743+jganz6@users.noreply.github.com>
2021-03-12 13:22:52 +0700

    add function openChat()

--- script/index.js
+++ script/index.js
@@ -1,4 +1,5 @@
 let status = true;
+let statusChat = true;
 
 function openNav() {
     btnNav = document.getElementById("btn-nav");
@@ -17,5 +18,25 @@ function openNav() {
         navSide.style.top = "0";
         navSide.style.display = "none";
         navSide.style.zIndex = "1";
+    }
+}
+
+function openChat() {
+    btnChat = document.getElementById("btn-chat");
+    chatSide = document.getElementById("chat-side");
+    if (statusChat === true) {
+        status = false;
+        chatSide.style.right = "-20px";
+        btnChat.style.marginRight = "230px";
+        chatSide.style.display = "flex";
+        chatSide.style.zIndex = "2";
+        chatSide.style.top = "-20px";
+    } else {
+        statusChat = true;
+        chatSide.style.right = "0";
+        btnChat.style.marginRight = "0";
+        chatSide.style.top = "0";
+        chatSide.style.display = "none";
+        chatSide.style.zIndex = "1";
     }
 }
